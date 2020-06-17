5106d0e3d055290ef13ced49c39ccd2a81442dfd
Vivian-Ellis <33746029+Vivian-Ellis@users.noreply.github.com>
2020-06-16 19:38:39 -0500

    Add files via upload

---
+++ PictureViewer/PictureViewer/Form1.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace PictureViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.SearchDirectory = new System.Windows.Forms.Button();
+            this.imageList = new System.Windows.Forms.ImageList(this.components);
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pauseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.webImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.imagesFromFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.howToToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusCurrentDirectory = new System.Windows.Forms.ToolStripStatusLabel();
+            this.pause = new System.Windows.Forms.Button();
+            this.WebSearchButton = new System.Windows.Forms.Button();
+            this.setTimerInterval = new System.Windows.Forms.NumericUpDown();
+            this.setTimerIntervalLabel = new System.Windows.Forms.Label();
+            this.seconds = new System.Windows.Forms.Label();
+            this.webURL = new System.Windows.Forms.RichTextBox();
+            this.webTimer = new System.Windows.Forms.Timer(this.components);
+            this.pictureViewer = new System.Windows.Forms.PictureBox();
+            this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip.SuspendLayout();
+            this.statusStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.setTimerInterval)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureViewer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SearchDirectory
+            // 
+            this.SearchDirectory.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+            this.SearchDirectory.Location = new System.Drawing.Point(71, 66);
+            this.SearchDirectory.Name = "SearchDirectory";
+            this.SearchDirectory.Size = new System.Drawing.Size(69, 38);
+            this.SearchDirectory.TabIndex = 1;
+            this.SearchDirectory.Text = "Search Directories";
+            this.SearchDirectory.UseVisualStyleBackColor = false;
+            this.SearchDirectory.Click += new System.EventHandler(this.SearchDirectory_Click);
+            // 
+            // imageList
+            // 
+            this.imageList.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 5000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.optionsToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(545, 24);
+            this.menuStrip.TabIndex = 6;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pauseToolStripMenuItem,
+            this.resetToolStripMenuItem,
+            this.webImageToolStripMenuItem,
+            this.imagesFromFileToolStripMenuItem});
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.O)));
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.optionsToolStripMenuItem.Text = "Options";
+            // 
+            // pauseToolStripMenuItem
+            // 
+            this.pauseToolStripMenuItem.Name = "pauseToolStripMenuItem";
+            this.pauseToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.P)));
+            this.pauseToolStripMenuItem.Size = new System.Drawing.Size(211, 22);
+            this.pauseToolStripMenuItem.Text = "Pause";
+            this.pauseToolStripMenuItem.Click += new System.EventHandler(this.stopTimer_Click);
+            // 
+            // webImageToolStripMenuItem
+            // 
+            this.webImageToolStripMenuItem.Name = "webImageToolStripMenuItem";
+            this.webImageToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.W)));
+            this.webImageToolStripMenuItem.Size = new System.Drawing.Size(211, 22);
+            this.webImageToolStripMenuItem.Text = "Images From Web";
+            this.webImageToolStripMenuItem.Click += new System.EventHandler(this.webImageToolStripMenuItem_Click);
+            // 
+            // imagesFromFileToolStripMenuItem
+            // 
+            this.imagesFromFileToolStripMenuItem.Name = "imagesFromFileToolStripMenuItem";
+            this.imagesFromFileToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F)));
+            this.imagesFromFileToolStripMenuItem.Size = new System.Drawing.Size(211, 22);
+            this.imagesFromFileToolStripMenuItem.Text = "Images From File";
+            this.imagesFromFileToolStripMenuItem.Click += new System.EventHandler(this.imagesFromFileToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.howToToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.H)));
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // howToToolStripMenuItem
+            // 
+            this.howToToolStripMenuItem.Name = "howToToolStripMenuItem";
+            this.howToToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.T)));
+            this.howToToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.howToToolStripMenuItem.Text = "How To";
+            this.howToToolStripMenuItem.Click += new System.EventHandler(this.howToToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.A)));
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.BackColor = System.Drawing.Color.LightGray;
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusCurrentDirectory});
+            this.statusStrip.Location = new System.Drawing.Point(0, 345);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(545, 22);
+            this.statusStrip.TabIndex = 7;
+            this.statusStrip.Text = "statusStrip1";
+            // 
+            // toolStripStatusCurrentDirectory
+            // 
+            this.toolStripStatusCurrentDirectory.BackColor = System.Drawing.Color.LightGray;
+            this.toolStripStatusCurrentDirectory.Name = "toolStripStatusCurrentDirectory";
+            this.toolStripStatusCurrentDirectory.Size = new System.Drawing.Size(172, 17);
+            this.toolStripStatusCurrentDirectory.Text = "toolStripStatusCurrentDirectory";
+            // 
+            // pause
+            // 
+            this.pause.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+            this.pause.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pause.Location = new System.Drawing.Point(15, 66);
+            this.pause.Name = "pause";
+            this.pause.Size = new System.Drawing.Size(49, 38);
+            this.pause.TabIndex = 8;
+            this.pause.Text = "Pause";
+            this.pause.UseVisualStyleBackColor = false;
+            this.pause.Click += new System.EventHandler(this.stopTimer_Click);
+            // 
+            // WebSearchButton
+            // 
+            this.WebSearchButton.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+            this.WebSearchButton.Location = new System.Drawing.Point(71, 66);
+            this.WebSearchButton.Name = "WebSearchButton";
+            this.WebSearchButton.Size = new System.Drawing.Size(50, 38);
+            this.WebSearchButton.TabIndex = 10;
+            this.WebSearchButton.Text = "Search Web";
+            this.WebSearchButton.UseVisualStyleBackColor = false;
+            this.WebSearchButton.Click += new System.EventHandler(this.WebSearchButton_Click);
+            // 
+            // setTimerInterval
+            // 
+            this.setTimerInterval.Location = new System.Drawing.Point(15, 40);
+            this.setTimerInterval.Maximum = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.setTimerInterval.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.setTimerInterval.Name = "setTimerInterval";
+            this.setTimerInterval.Size = new System.Drawing.Size(53, 20);
+            this.setTimerInterval.TabIndex = 11;
+            this.setTimerInterval.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.setTimerInterval.ValueChanged += new System.EventHandler(this.setTimerInterval_ValueChanged);
+            // 
+            // setTimerIntervalLabel
+            // 
+            this.setTimerIntervalLabel.AutoSize = true;
+            this.setTimerIntervalLabel.Location = new System.Drawing.Point(12, 24);
+            this.setTimerIntervalLabel.Name = "setTimerIntervalLabel";
+            this.setTimerIntervalLabel.Size = new System.Drawing.Size(93, 13);
+            this.setTimerIntervalLabel.TabIndex = 12;
+            this.setTimerIntervalLabel.Text = "Set Timer Interval:";
+            // 
+            // seconds
+            // 
+            this.seconds.AutoSize = true;
+            this.seconds.Location = new System.Drawing.Point(74, 42);
+            this.seconds.Name = "seconds";
+            this.seconds.Size = new System.Drawing.Size(47, 13);
+            this.seconds.TabIndex = 13;
+            this.seconds.Text = "seconds";
+            // 
+            // webURL
+            // 
+            this.webURL.Location = new System.Drawing.Point(12, 110);
+            this.webURL.Name = "webURL";
+            this.webURL.Size = new System.Drawing.Size(164, 162);
+            this.webURL.TabIndex = 14;
+            this.webURL.Text = "";
+            // 
+            // webTimer
+            // 
+            this.webTimer.Interval = 5000;
+            this.webTimer.Tick += new System.EventHandler(this.webTimer_Tick);
+            // 
+            // pictureViewer
+            // 
+            this.pictureViewer.Location = new System.Drawing.Point(182, 40);
+            this.pictureViewer.Name = "pictureViewer";
+            this.pictureViewer.Size = new System.Drawing.Size(350, 283);
+            this.pictureViewer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureViewer.TabIndex = 3;
+            this.pictureViewer.TabStop = false;
+            // 
+            // resetToolStripMenuItem
+            // 
+            this.resetToolStripMenuItem.Name = "resetToolStripMenuItem";
+            this.resetToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.X)));
+            this.resetToolStripMenuItem.Size = new System.Drawing.Size(211, 22);
+            this.resetToolStripMenuItem.Text = "Reset";
+            this.resetToolStripMenuItem.Click += new System.EventHandler(this.ClearAll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.OldLace;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(545, 367);
+            this.Controls.Add(this.webURL);
+            this.Controls.Add(this.seconds);
+            this.Controls.Add(this.setTimerIntervalLabel);
+            this.Controls.Add(this.setTimerInterval);
+            this.Controls.Add(this.WebSearchButton);
+            this.Controls.Add(this.pause);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.menuStrip);
+            this.Controls.Add(this.pictureViewer);
+            this.Controls.Add(this.SearchDirectory);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "Form1";
+            this.Text = "PictureViewer";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.setTimerInterval)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureViewer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button SearchDirectory;
+        private System.Windows.Forms.ImageList imageList;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.PictureBox pictureViewer;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusCurrentDirectory;
+        private System.Windows.Forms.Button pause;
+        private System.Windows.Forms.Button WebSearchButton;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pauseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem webImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem imagesFromFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem howToToolStripMenuItem;
+        private System.Windows.Forms.NumericUpDown setTimerInterval;
+        private System.Windows.Forms.Label setTimerIntervalLabel;
+        private System.Windows.Forms.Label seconds;
+        private System.Windows.Forms.RichTextBox webURL;
+        private System.Windows.Forms.Timer webTimer;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
+    }
+}
+
---
+++ PictureViewer/PictureViewer/Form1.cs
@@ -0,0 +1,274 @@
+﻿/* 
+ * Vivian Ellis final project
+ * CSCD 371 Picture Viewer
+ * Summer 2017
+ * 
+ * This program lets a user specify a directory
+ * or a list of pictures from the web
+ * if not pictures exist in directory nothing will be displayed
+ * similarly if no pictures exist on web URL an 'X' image will be
+ * displayed letting user know that the URL does not contain an image
+ * 
+ * the last picture appears for double time and then the cycle starts
+ * over again. There is no transition between pictures.
+ * 
+ * A file 'pictures' includes 5 pictures of type .gif,.bmp, and .jpg 
+ *
+ * A text file includes random pictures from the web. Copy paste to
+ * test. Use keyboard short cut to paste into the rich text box 'webURL'.
+ * Make sure to enter after each entry.
+ * 
+ * The form adjusts to picture size, however if the picture is 
+ * bigger than the users screen you can move the form to view
+ * the rest of the picture, no scroll bar will be included.
+ * 
+ * see Help -> About for more specifications on how to run the program
+ */
+
+
+using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace PictureViewer
+{
+    public partial class Form1 : Form
+    {
+        //where the user wants to grab the pictures from
+        private string path = "";
+
+        //all the pictures in that path
+        private string[] pictures;
+
+        //i just tells us which picture we are currently looking at
+        private int i = 0;
+
+        //is user searching the web?
+        private bool isWebSearch = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            toolStripStatusCurrentDirectory.Text = "";
+            timer.Interval= (int)setTimerInterval.Value*1000;
+            webTimer.Interval = (int)setTimerInterval.Value * 1000;
+
+            //have to select which method to search by before begining
+            WebSearchButton.Visible = false;
+            pause.Visible = false;
+            pauseToolStripMenuItem.Enabled = false;
+            SearchDirectory.Visible = false;
+            webURL.Visible = false;
+            setTimerInterval.Enabled = false;
+        }
+
+        //user wants to look for a directory
+        private void SearchDirectory_Click(object sender, EventArgs e)
+        {
+            isWebSearch = false;
+            pause.Enabled = true;
+            pauseToolStripMenuItem.Enabled = true;
+            //the three types of images we will look for
+            string[] jpg;
+            string[] gif;
+            string[] bmp;
+
+            //start from the beginning
+            i = 0;
+
+            FolderBrowserDialog directory = new FolderBrowserDialog();
+            if (directory.ShowDialog() == DialogResult.OK)
+            {
+                //grab the directory
+                path = directory.SelectedPath;
+                toolStripStatusCurrentDirectory.Text = path;
+
+                jpg = System.IO.Directory.GetFiles(path, "*.jpg");
+                gif = System.IO.Directory.GetFiles(path, "*.gif");
+                bmp = System.IO.Directory.GetFiles(path, "*.bmp");
+
+                //merge them together...
+                pictures = jpg.Concat(gif.Concat(bmp).ToArray()).ToArray();
+
+                //there is nothing to look at
+                if (pictures.Length == 0) return;
+
+                //show the first picture right away
+                Image image = Image.FromFile(pictures[0]);
+                pictureViewer.Image = image;
+
+                //move to the next picture
+                i++;
+
+                //start the timer
+                timer.Enabled = true;
+                timer.Start();
+
+            }//end if
+        }//end SearchDirectory_Click
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            //we will cycle through the pictures until the user says stop
+            if (i < pictures.Length)
+            {
+                Image image = Image.FromFile(pictures[i]);
+                pictureViewer.Width = image.Width;
+                pictureViewer.Height = image.Height;
+                pictureViewer.Image = image;
+                i++;
+            }
+            else
+                i = 0;
+        }//end timer_Tick
+
+        //pause the slide show
+        private void stopTimer_Click(object sender, EventArgs e)
+        {
+            pause.Enabled = false;
+            pauseToolStripMenuItem.Enabled = false;
+
+            if (isWebSearch == true)
+            {
+                webTimer.Enabled = false;
+                webTimer.Stop();
+            }
+            else
+            {
+                timer.Enabled = false;
+                timer.Stop();
+            }
+        }//end stopTimer_Click
+
+        //view pictures from the web
+        private void WebSearchButton_Click(object sender, EventArgs e)
+        {
+            //user didnt input anything
+            if (webURL.Lines.Length == 0) return;
+
+            i = 0;
+            pause.Enabled = true;
+            pauseToolStripMenuItem.Enabled = true;
+
+            //display first image right away
+            pictureViewer.ImageLocation = webURL.Lines[i];
+            toolStripStatusCurrentDirectory.Text = webURL.Lines[i];
+
+            //move to the next picture
+            i++;
+
+            //start the webTimer
+            webTimer.Enabled = true;
+            webTimer.Start();
+        }//end WebSearchButton_Click
+
+        //private void Transition(Image image)
+        //{
+        //    Graphics g = Graphics.FromImage(image);
+       //}
+
+        private void setTimerInterval_ValueChanged(object sender, EventArgs e)
+        {
+            webTimer.Interval = (int)setTimerInterval.Value * 1000;
+            timer.Interval = (int)setTimerInterval.Value * 1000;
+        }//end setTimerInterval_ValueChanged
+
+        //user wants to search their computer
+        private void imagesFromFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webTimer.Enabled = false;
+            webTimer.Stop();
+            webURL.Text = "";
+            WebSearchButton.Visible = false;
+
+            pause.Visible = true;
+            pause.Enabled = false;
+            pauseToolStripMenuItem.Enabled = false;
+
+            timer.Enabled = false;
+            timer.Stop();
+            SearchDirectory.Visible = true;
+            webURL.Visible = false;
+            setTimerInterval.Enabled = true;
+            pictureViewer.Image = null;
+        }//end imagesFromFileToolStrinpMenuItem_Click
+
+        //user wants to search the web
+        private void webImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //searching the web
+            isWebSearch = true;
+
+            timer.Enabled = false;
+            timer.Stop();
+            WebSearchButton.Visible = true;
+            pause.Visible = true;
+            pause.Enabled = false;
+            pauseToolStripMenuItem.Enabled = false;
+
+            webTimer.Enabled = false;
+            webTimer.Stop();
+            SearchDirectory.Visible = false;
+            webURL.Visible = true;
+            setTimerInterval.Enabled = true;
+            pictureViewer.Image = null;
+            webURL.Text = "";
+        }//end webImageToolStripMenuItem_Click
+
+        private void webTimer_Tick(object sender, EventArgs e)
+        {
+            foreach(string s in webURL.Lines)
+            {
+                if (i < webURL.Lines.Length)
+                {
+                    pictureViewer.ImageLocation = webURL.Lines[i];
+                    toolStripStatusCurrentDirectory.Text = webURL.Lines[i];
+
+                    //move to the next picture
+                    i++;
+                }//end if
+                else
+                    i = 0;
+            }//end foreach
+        }//end webTimer_Tick
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Developer viv\nversion 1.0\nPlatform target: any CPU - Preferred 32-bit\nTarget framework: .NET Framework 4.5.2");
+        }//end about
+
+        //show to 'rules'
+        private void howToToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           MessageBox.Show("Welcome to Picture Viewer!\n\nIn this program you will first select where you would like to view pictures(via web URL or file).\nUser must select either option from the 'Options' menu.\n         If the user selects 'Images From Web' a text box appears and allows the user to enter the web address of a picture. Hit enter after each URL and leave no double spacing\n          If the user chooses 'Images From File' a 'Search Directory' button will appear letting the user chose from their computer. All pictures of type .jpg, .gif, and .bmp will be displayed\nAt anytime the timer may be set between 1 - 60 seconds. The default viewing time is 5 seconds.");
+        }//end rules
+
+        //clears all pictures currently showing
+        private void ClearAll(object sender, EventArgs e)
+        {
+            toolStripStatusCurrentDirectory.Text = "";
+            webURL.Text = "";
+
+            //reset everything to beginning
+            WebSearchButton.Visible = false;
+            pause.Visible = false;
+            pauseToolStripMenuItem.Enabled = false;
+            SearchDirectory.Visible = false;
+            webURL.Visible = false;
+            setTimerInterval.Enabled = false;
+
+            setTimerInterval.Value = 5;
+            webTimer.Interval = 5000;
+            timer.Interval = 5000;
+
+            timer.Enabled = false;
+            timer.Stop();
+
+            webTimer.Enabled = false;
+            webTimer.Stop();
+
+            pictureViewer.Image = null;
+        }//end ClearAll
+    }//end form class
+}//end name space PictureViewer
